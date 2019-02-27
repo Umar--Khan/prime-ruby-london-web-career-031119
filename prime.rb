117226ef5f7f8e608c21412a2ac759c9de14da56
Umar Khan <umarkhandev@gmail.com>
2019-02-27 17:22:18 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,10 @@
 # Add  code here!
-def prime?(array)
-  (2..array).none? {|n|array % n == 0}
+def prime? (n)
+    if n <= 1
+        false
+    elsif n == 2
+        true
+    else
+        (2..n/2).none? { |i| n % i == 0}
+    end
 end
